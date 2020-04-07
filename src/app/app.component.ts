6cab6dea971c1fb70d52a1660299876f5940ffa0
GHANEM <mohsenghanem02@gmail.com>
2020-04-07 13:44:08 +0200

    Operator filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 
  import { Component } from '@angular/core';
- import { of, concat, throwError } from 'rxjs';
- import { startWith, catchError } from 'rxjs/operators';
+ import { of, concat, from , throwError } from 'rxjs';
+ import { startWith, catchError, filter } from 'rxjs/operators';
 
 
 /*concat operator*/
@@ -11,20 +11,21 @@ const S2 =of(4,5,6);
 
 /* Start with operator */
 const source = of(1,2,3);
-const example = source.pipe(startWith(0));
-//const subscribe = example.subscribe(val => console.log(val));
-
+const example = source.pipe(startWith(0)).subscribe(val => console.log(val))
 
 /* catch / catchError operator  */
 const source1 =throwError('this is an error');
-const example1 = source1.pipe(catchError(val1 => of(`I caught: ${val1}`)));
-const subcribe1= example1.subscribe(val1 => console.log(val1));
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+const example1 = source1.pipe(catchError(val1 => of(`I caught: ${val1}`))).subscribe(val1 => console.log(val1));
 
+/* filter operator */
+const source2 = from([1, 2, 3, 4, 5]);
+const example2 = source2.pipe(filter(num => num === 2)).subscribe(val3 => console.log(`Affichage de filter : ${val3}`));
+
+@Component({
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
+})
 export class AppComponent {
 
 }
