f718e8b0600ca8fd7cdf068e531a990a15016123
GHANEM <mohsenghanem02@gmail.com>
2020-04-07 14:47:08 +0200

    Operator first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,30 @@
 
  import { Component } from '@angular/core';
  import { of, concat, from , throwError } from 'rxjs';
- import { startWith, catchError, filter } from 'rxjs/operators';
+ import { startWith, catchError, filter, first } from 'rxjs/operators';
 
 
-/*concat operator*/
+/* concat operator */
 const S1=of(1,2,3);
 const S2 =of(4,5,6);
 //concat(S1,S2).subscribe(console.log);
 
 /* Start with operator */
 const source = of(1,2,3);
-const example = source.pipe(startWith(0)).subscribe(val => console.log(val))
+//const example = source.pipe(startWith(0)).subscribe(val => console.log(val))
 
-/* catch / catchError operator  */
+/* catch/catchError operator */
 const source1 =throwError('this is an error');
-const example1 = source1.pipe(catchError(val1 => of(`I caught: ${val1}`))).subscribe(val1 => console.log(val1));
+//const example1 = source1.pipe(catchError(val1 => of(`I caught: ${val1}`))).subscribe(val1 => console.log(val1));
 
 /* filter operator */
 const source2 = from([1, 2, 3, 4, 5]);
-const example2 = source2.pipe(filter(num => num === 2)).subscribe(val3 => console.log(`Affichage de filter : ${val3}`));
+//const example2 = source2.pipe(filter(num => num === 2)).subscribe(val3 => console.log(`Affichage de filter : ${val3}`));
+
+/* first operator */
+const source3 =from([1,2,3,4,5,6]);
+const subscribe3 =source3.pipe(first())
+.subscribe(data => console.log(`Affichage de la liste des elements : ${data}`));
 
 @Component({
    selector: 'app-root',
