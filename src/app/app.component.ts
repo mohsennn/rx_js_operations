40445ba1c29f2527390a06609d5ea2f1daa8d035
GHANEM <mohsenghanem02@gmail.com>
2020-04-07 12:00:38 +0200

    Operator combination :startWith

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 
  import { Component } from '@angular/core';
  import { of, concat } from 'rxjs';
+ import { startWith } from 'rxjs/operators';
 
 
-concat(of(1,2,3),
-of(4,5,6),
-of(7,8,9))
-.subscribe(console.log);
+/*concat operator */
+const S1=of(1,2,3);
+const S2 =of(4,5,6);
+//concat(S1,S2).subscribe(console.log);
 
-
+/*Start with operator */
+const source = of(1,2,3);
+const example = source.pipe(startWith(0));
+const subscribe = example.subscribe(val => console.log(val));
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
