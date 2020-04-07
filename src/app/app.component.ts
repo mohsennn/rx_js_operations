7fa82057123345bb3fd655c95182ae85d5cff1fd
GHANEM <mohsenghanem02@gmail.com>
2020-04-07 15:21:43 +0200

    Operator map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 
  import { Component } from '@angular/core';
  import { of, concat, from , throwError } from 'rxjs';
- import { startWith, catchError, filter, first } from 'rxjs/operators';
+ import { startWith, catchError, filter, first,map } from 'rxjs/operators';
 
 
 /* concat operator */
@@ -14,17 +14,21 @@ const source = of(1,2,3);
 //const example = source.pipe(startWith(0)).subscribe(val => console.log(val))
 
 /* catch/catchError operator */
-const source1 =throwError('this is an error');
+const source1 = throwError('this is an error');
 //const example1 = source1.pipe(catchError(val1 => of(`I caught: ${val1}`))).subscribe(val1 => console.log(val1));
 
 /* filter operator */
-const source2 = from([1, 2, 3, 4, 5]);
+const source2 = from ([1, 2, 3, 4, 5]);
 //const example2 = source2.pipe(filter(num => num === 2)).subscribe(val3 => console.log(`Affichage de filter : ${val3}`));
 
 /* first operator */
-const source3 =from([1,2,3,4,5,6]);
-const subscribe3 =source3.pipe(first())
-.subscribe(data => console.log(`Affichage de la liste des elements : ${data}`));
+const source3 = from ([1,2,3,4,5,6]);
+//const subscribe3 = source3.pipe(first()).subscribe(data => console.log(`Affichage de la liste des elements : ${data}`));
+
+/* map operator */
+const source4 = from([1,2,3,4,5]);
+/*add 10  to each element */
+const subscribe4 =source4.pipe(map(item => item + 10)).subscribe(val => console.log(`after addition : ${val}`));
 
 @Component({
    selector: 'app-root',
