4e85ae64bed496a37050800c05dbadfffe2e9f05
GHANEM <mohsenghanem02@gmail.com>
2020-04-07 11:27:24 +0200

    Operator combination :contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+
+ import { Component } from '@angular/core';
+ import { of, concat } from 'rxjs';
+
+
+concat(of(1,2,3),
+of(4,5,6),
+of(7,8,9))
+.subscribe(console.log);
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'concat';
+
 }
